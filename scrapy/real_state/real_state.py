522e78b2d3e90f741b94b3326ef89970f251e48a
subratamondal1 <subratasubha2@gmail.com>
2023-01-06 16:53:45 +0530

    scrapy real estate

---
+++ scrapy/real_state/real_state.py
@@ -0,0 +1,38 @@
+import scrapy
+from scrapy.crawler import CrawlerProcess
+import json
+
+
+class PythonListingSpider(scrapy.Spider):
+    name = "pythonlistingsspider"
+    start_urls = ["https://www.arizonarealestate.com/maricopa/"]
+    found_listings = []
+
+    def parse(self, response):
+        gallery = response.xpath("//div[@class='si-listings-column']")
+
+        for listing in gallery:
+            listing_details = dict()
+
+            listing_details["name"] = listing.xpath(
+                "//div[@class='si-listing__title-main']/text()").get()
+
+            listing_details["description"] = listing.xpath(
+                "//div[@class='si-listing__title-description']/text()").get()
+
+            listing_details["price"] = listing.xpath(
+                "//div[@class='si-listing__photo-price']/span/text()").get()
+
+            listing_details["agency"] = listing.xpath(
+                "//div[@class='si-listing__footer']/div/text()").get()
+
+            self.found_listings.append(listing_details)
+
+
+if __name__ == "main":
+    process = CrawlerProcess({"LOG_LEVEL": "ERROR"})
+    process.crawl(PythonListingSpider)
+    spider = next(iter(process.crawlers)).spider
+    process.start()
+
+    print(json.dumps(PythonListingSpider.found_listings, indent=4))
